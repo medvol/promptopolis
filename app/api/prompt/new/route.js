b077ef889ed95e2e8f1f72e853437afcd8dd8d12
Oleh Medvid <meandjob@protonmail.com>
2023-05-22 22:18:22 +0300

    add: PromptCard component

--- app/api/prompt/new/route.js
+++ app/api/prompt/new/route.js
@@ -1,10 +1,8 @@
 import Prompt from "@models/prompt";
 import { connectToDB } from "@utils/database";
 
-export const POST = async (req) => {
-    const { userId, prompt, tag } = await req.json();
-    
-    console.log(req)
+export const POST = async (request) => {
+  const { userId, prompt, tag } = await request.json();
 
   try {
     await connectToDB();
