dbf0940f06d8da4a55e593fdd5d3dbea1a72f74d
Oleh Medvid <meandjob@protonmail.com>
2023-05-25 19:15:45 +0300

    update: gitignore file

--- app/profile/page.jsx
+++ app/profile/page.jsx
@@ -23,7 +23,9 @@ const MyProfile = () => {
     if (session?.user.id) fetchPosts();
   }, [session?.user.id]);
 
-  const handleEdit = (post) => {};
+  const handleEdit = (post) => {
+        router.push(`/update-prompt?id=${post._id}`);
+  };
 
   const handleDelete = async (post) => {};
 
